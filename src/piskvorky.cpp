46b1d90330fa771e57324c8e282d6a98987d2949
Jan Noha <nohajc@gmail.com>
2017-04-14 20:54:07 -0400

    Added simple UI grid with onclick event returning coordinates.

--- src/piskvorky.cpp
+++ src/piskvorky.cpp
@@ -1,4 +1,6 @@
+#include <cstdio>
 #include <sciter-x-window.hpp>
+
 
 static RECT wrc = { 100, 100, 800, 800 };
 
@@ -7,18 +9,27 @@ public:
   frame() : window( SW_MAIN | SW_TITLEBAR | SW_CONTROLS | SW_ENABLE_DEBUG, wrc) {}
 
   BEGIN_FUNCTION_MAP
-    FUNCTION_0("architecture", architecture);
+      FUNCTION_2("ui_click", ui_click);
+
   END_FUNCTION_MAP
 
-  int architecture() {
-    // this function is here just for the demo purposes,
-    // it shows native function callable from script as view.architecture();
-#if defined(TARGET_32)
-    return 32;
-#elif defined(TARGET_64)
-    return 64;
-#endif
-  }
+    sciter::value ui_click(sciter::value cur_x, sciter::value cur_y) {
+        int x = cur_x.get<int>();
+        int y = cur_y.get<int>();
+
+        printf("Clicked at %d, %d\n", x, y);
+        return sciter::value();
+    }
+//
+//  int architecture() {
+//    // this function is here just for the demo purposes,
+//    // it shows native function callable from script as view.architecture();
+//#if defined(TARGET_32)
+//    return 32;
+//#elif defined(TARGET_64)
+//    return 64;
+//#endif
+//  }
 };
 
 #include "resources.h" // packed /res/ folder
